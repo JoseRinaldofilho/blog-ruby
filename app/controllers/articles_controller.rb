a50dc701267d8816e7a6701c908243eb7210c4fb
Rinaldo <mundial.cel11@gmail.com>
2022-02-17 23:14:38 -0300

    criado a show e atualizado

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,8 @@ class ArticlesController < ApplicationController
     #busca todas os dados articles no bano an variavel usa @
     @articles = Article.all  #fica publica na minha view
   end
+  def show
+    @articles = Article.find(params[:id]) # find busca um parametro e passo o id
+  end
 end
+
